efcc28afe71975089eadf6f78c59cb79bd03065b
kaval <kaval.itg@gmail.com>
2024-05-24 13:42:27 +0200

    Added return button to about page

--- app/src/main/java/com/example/project/AboutPage.java
+++ app/src/main/java/com/example/project/AboutPage.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,8 +19,18 @@ public class AboutPage  extends AppCompatActivity {
 
         WebView myWebView = findViewById(R.id.my_webview);
         WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript
+        myWebView.addJavascriptInterface(new WebAppInterface(), "Android");
         myWebView.setWebViewClient(new WebViewClient()); // Do not open in Chrome!
         myWebView.loadUrl("file:///android_asset/index.html"); // Load local HTML file
 
+    }
+    public class WebAppInterface {
+        @JavascriptInterface
+        public void goToMainActivity() {
+            // Navigate back to MainActivity
+            Intent intent = new Intent(AboutPage.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
